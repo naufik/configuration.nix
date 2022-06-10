506490a2fffa3b021329806a3223b47c7cecf490
Naufal Fikri <naufal@naufik.net>
2022-06-10 16:37:56 +1000

    Add ability to run nixos-rebuild straight away.

--- apply.sh
+++ apply.sh
@@ -5,3 +5,12 @@ fi
 
 rsync --recursive --delete ./nix/ /etc/nixos
 echo "[ack] config applied to /etc/nixos in local machine"
+
+if [ "$1" = "switch" ]; then
+  nixos-rebuild switch
+  echo "[ack] switched to new config"
+elif [ "$1" = "boot" ]; then
+  nixos-rebuild boot
+  echo "[ack] new config will be activated on next boot"
+fi
+
