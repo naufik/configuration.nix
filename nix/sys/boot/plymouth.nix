d93723aeb9e7863cba5da2af9dc79d1e9176e4fe
Naufal Fikri <naufal@naufik.net>
2022-06-27 19:06:00 +1000

    Use framework laptop logo for plymouth boot.

--- nix/sys/boot/plymouth.nix
+++ nix/sys/boot/plymouth.nix
@@ -10,6 +10,12 @@ with lib;
     boot.plymouth = {
       enable = true;
       theme = "breeze";
+
+      # Disclaimer: I am not affiliated with Framework Computer Inc.
+      logo = pkgs.fetchurl {
+        url = "https://naufik.net/stash/framework-logo-white.png";
+        sha256 = "1n6y2840cf53kgrcrhjjvr3pzdm0fa52hr8khxss21r6k261j6h9";
+      };
     };
 
     boot.initrd.systemd.enable = true;
